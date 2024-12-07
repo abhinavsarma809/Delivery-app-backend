92c47908130d4280899eb9dc8ec65af44c034ed2
Bhamidipati Sarma <abhinavsarma245@gmail.com>
2024-12-07 13:58:31 +0530

    changew

--- index.js
+++ index.js
@@ -6,29 +6,33 @@ const cors = require("cors");
 
 env.config();
 
+const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
 const userRoutes = require('./api/user');
 const foodRoutes = require('./api/home');
 
-// Middleware
-app.use(cors({ origin: "*" }));
+app.use(cors({
+    origin: "*"
+}));
+
 app.use(express.json());
 
-// MongoDB connection
-mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Routes
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+
 app.use("/api/user", userRoutes);
 app.use("/api/food", foodRoutes);
 
-// Export the app for Vercel
-module.exports = app;
+
+mongoose.connection.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
