9abcfadcd4df7ea7ea4290e20982a81636ff7b0b
Bhamidipati Sarma <abhinavsarma245@gmail.com>
2024-12-08 21:59:37 +0530

    changew

--- index.js
+++ index.js
@@ -3,31 +3,32 @@ const express = require("express");
 const app = express();
 const env = require("dotenv");
 const cors = require("cors");
-
-env.config();
-
-const PORT = process.env.PORT || 3000;
-const MONGO_URL = process.env.MONGO_URL;
 const userRoutes = require('./api/user');
 const foodRoutes = require('./api/home');
 
+env.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+
+// Middleware setup
 app.use(cors({
     origin: "*"
 }));
-
 app.use(express.json());
-
-app.listen(PORT,()=>{
-    console.log(`server is running in${PORT}`);
-    mongoose.connect(MONGO_URL).then(()=>console.log("connected to mongoose")).catch((err)=> console.log(err));
-});
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-
 app.use("/api/user", userRoutes);
 app.use("/api/food", foodRoutes);
 
+// MongoDB connection
+mongoose.connect(MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
+// Vercel needs the handler to be exported for serverless functions
+module.exports = (req, res) => {
+    app(req, res); // Pass the request and response to the app
+};
