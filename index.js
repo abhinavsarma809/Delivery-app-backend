8ec012fec5ccbbc930f2c127c6f22a0e62cd0d0a
Bhamidipati Sarma <abhinavsarma245@gmail.com>
2024-12-10 14:41:47 +0530

    Set Node.js engine to 22.x

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ const MONGO_URL = process.env.MONGO_URL;
 
 
 app.use(cors({
-    origin: "*"
-}));
+    origin: "*", // You can use your Vercel URL instead of "*" for more security.
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
+  
 app.use(express.json());
 app.listen(Port,()=>{
     console.log(`Server is running on port ${Port}`);
